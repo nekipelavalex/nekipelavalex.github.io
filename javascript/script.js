6cafc2dc0a868b1be19588dcfb10408cfde6025e
example1 <bonia1991@gmail.com>
2019-01-17 15:11:58 +0300

    home paige carousel

--- javascript/script.js
+++ javascript/script.js
@@ -66,3 +66,31 @@ $(function () {
         });
     });
 });
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop: true,
+    margin: 10,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        960: {
+            items: 2
+        },
+        1200: {
+            items: 2
+        }
+    }
+});
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY > 0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
