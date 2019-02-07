05a8e50cd0e319320f077ca82bf46577f3b22f92
example1 <bonia1991@gmail.com>
2019-02-07 21:25:31 +0300

    add skills

--- javascript/script.js
+++ javascript/script.js
@@ -89,3 +89,13 @@ owl.on('mousewheel', '.owl-stage', function (e) {
     }
     e.preventDefault();
 });
+
+function externalLinks() {
+    links = document.getElementsByTagName("a");
+    for (i = 0; i < links.length; i++) {
+        link = links[i];
+        if (link.getAttribute("href") && link.getAttribute("rel") == "external")
+            link.target = "_blank";
+    }
+}
+window.onload = externalLinks;
