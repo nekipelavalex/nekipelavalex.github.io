824cf827404a38716a3eaa651c0b2292e2410d37
Alex <bonia1991@gmail.com>
2021-06-11 12:24:26 +0300

    correct 062021

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,15 @@
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     closeElem = document.querySelector('.menu__close');
+    closeElemOverlay = document.querySelector('.menu__overlay');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
 closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+closeElemOverlay.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
